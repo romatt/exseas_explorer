c75c0f04bebd99e1a59601aeff8657434d529f91
Roman <atroman@env.ethz.ch>
2022-03-17 06:51:03 +0000

    First attempt to overlay some data on map

--- exseas_explorer/app.py
+++ exseas_explorer/app.py
@@ -1,34 +1,35 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
-from dash import Dash, html, dcc
-import plotly.express as px
-import pandas as pd
+# import plotly.express as px
+# import pandas as pd
+import dash
+import dash_leaflet as dl
+import geopandas
+from dash import Dash, dcc, html
+
+filename = "/ytpool/data/ETH/INTEXseas/land_patches_40y_era5_RTOT_djf_ProbDry.geojson"
+file = open(filename)
+df = geopandas.read_file(file)
+
+filtered = df[df['key'] < 1952]
+geo_j = filtered['geometry'].to_json()
 
 app = Dash(__name__)
-
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
-
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for your data.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
+app.layout = html.Div([
+    dl.Map(center=[0, 0], zoom=2,
+           children=[dl.TileLayer(),
+           dl.GeoJSON(data=geo_j, id="patches")],
+           style={
+               'width': '90%',
+               'height': '70vh',
+               'margin': "auto",
+               "display": "block"
+           },
+           id="map")
 ])
+
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
