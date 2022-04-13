8352fc73eb71d25b8db9c9885d089f9f5300c793
Roman <atroman@env.ethz.ch>
2022-04-13 20:43:15 +0000

    Update colorbar according to colors from Maxi

--- exseas_explorer/app.py
+++ exseas_explorer/app.py
@@ -12,11 +12,15 @@ import dash_leaflet as dl
 import dash_leaflet.express as dlx
 import geopandas
 import matplotlib
+import matplotlib.pyplot as plt
 import numpy as np
 from dash import Dash, dcc, html
 from dash.dependencies import Input, Output
+from matplotlib.colors import BoundaryNorm, ListedColormap
 
 # OPTIONS
+MIN_YEAR = 1950
+MAX_YEAR = 2020
 DATA_DIR = '/ytpool/data/ETH/INTEXseas'
 PARAMETER_LIST = [
     {
@@ -89,6 +93,23 @@ RANKING_LIST = [{
     'value': 2
 }]
 
+# COLORMAP
+greys=plt.cm.Greys    # 1950er
+pinks=plt.cm.RdPu     # 1960er
+purp = plt.cm.Purples # 1970er
+blues=plt.cm.Blues    # 1980er
+greens=plt.cm.Greens  # 1990er
+ylors=plt.cm.Oranges #YlOrBr   # 2000er 
+reds=plt.cm.Reds      # 2010er 
+cols=ListedColormap([greys(20),greys(40),greys(60),greys(80),greys(100),greys(120),greys(140),greys(160),greys(180),greys(200),   \
+             pinks(20),pinks(40),pinks(60),pinks(80),pinks(100),pinks(120),pinks(140),pinks(160),pinks(180),pinks(200),   \
+                     purp(20),purp(40),purp(60),purp(80),purp(100),purp(120),purp(140),purp(160),purp(180),purp(200),   \
+                     blues(20),blues(40),blues(60),blues(80),blues(100),blues(120),blues(140),blues(160),blues(180),blues(200),   \
+                     greens(20),greens(40),greens(60),greens(80),greens(100),greens(120),greens(140),greens(160),greens(180),greens(200),\
+                     ylors(20),ylors(40),ylors(60),ylors(80),ylors(100),ylors(120),ylors(140),ylors(160),ylors(180),ylors(200),\
+                    reds(20),reds(40),reds(60),reds(80),reds(100),reds(120),reds(140),reds(160),reds(180),reds(200),\
+                     plt.cm.YlOrRd(60)])
+norm=BoundaryNorm(np.arange(1950,2020+1,1),cols.N)
 
 # FUNCTION DEFINITONS
 def load_patches(path: str) -> geopandas.GeoDataFrame:
@@ -124,18 +145,21 @@ default_patches = filter_patches(default_patches, 1)
 
 def generate_cbar(labels: list) -> dl.Colorbar:
     """
-    Generate colorbar for provided labels
+    Generate colorbar for provided year labels
+
+    Test years
+    years = [1952,1963,1964,1966,1969,1971,1979,1983,1987,2010]
+
+    Parameters
+    ----------
+    labels : list
+        List of years to process
     """
 
     # Define colors
-    # classes = np.arange(np.nanmin(labels), np.nanmax(labels), 1)
-    cmap = matplotlib.cm.get_cmap('Spectral', len(labels))
-    colors = [cmap(x / np.max(labels))
-              for x in labels]  # Assign colors to labels
-    colors = [matplotlib.colors.to_hex(x)
-              for x in colors]  # Convert colors to hex
-
-    # Create colorbar.
+    colors = [matplotlib.colors.to_hex(cols(norm(x))) for x in labels]
+    
+    # Create colorbar
     ctg = [
         "{}".format(cls, labels[i + 1]) for i, cls in enumerate(labels[:-1])
     ] + ["{}".format(labels[-1])]
@@ -297,10 +321,10 @@ def draw_patches(parameter_value, parameter_option, season_value,
     patches = load_patches(os.path.join(DATA_DIR, selected_file))
     patches = filter_patches(patches, ranking_option)
 
-    print(patches['lab'])
+    print(patches['key'])
 
     # Update colorbar
-    colorbar = generate_cbar(list(patches['lab']))
+    colorbar = generate_cbar(list(patches['key']))
 
     return patches.__geo_interface__, parameter_options, option_selected, colorbar
 
