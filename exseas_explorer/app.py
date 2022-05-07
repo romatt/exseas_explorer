d67972d7c2ee37a8625274694cb6bdc1ae51f585
Roman <atroman@env.ethz.ch>
2022-05-07 11:45:30 +0000

    Remove unused imports

--- exseas_explorer/app.py
+++ exseas_explorer/app.py
@@ -1,19 +1,13 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
-import base64
 import os
 
-# import plotly.express as px
-# import pandas as pd
 import dash
 import dash_bootstrap_components as dbc
 import dash_leaflet as dl
 import dash_leaflet.express as dlx
-# import geopandas
-# import matplotlib
-import numpy as np
-from dash import Dash, State, dash_table, dcc, html
+from dash import Dash, dcc, html
 from dash.dependencies import Input, Output
 from dash_extensions.javascript import assign
 
