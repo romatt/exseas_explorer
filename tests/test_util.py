68ea04832431b28964dd697ee9e59f60d6ae59aa
Roman <roman@kyril.pro>
2022-11-27 16:56:56 +0000

    Add test for generate_poly

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,7 +1,13 @@
 import pytest
 import numpy as np
+import geojson
 
-from exseas_explorer.util import filter_patches, generate_cbar, generate_table
+from exseas_explorer.util import (
+    filter_patches,
+    generate_cbar,
+    generate_table,
+    generate_poly,
+)
 
 
 def test_load_patches(default_patches):
@@ -43,3 +49,11 @@ def test_generate_table(filtered_patches, colorscale):
     )
     assert poly_table.data[0] == {"year": 1988, "Area (km^2)": 7957515.04}
     assert poly_table.data[-1] == {"year": 1988, "Area (km^2)": 617857.55}
+
+
+def test_generate_poly():
+    polygon = generate_poly([-180, 180], [0, 90])
+    assert type(polygon) == geojson.feature.FeatureCollection
+    assert len(polygon.features) == 1
+    assert polygon.features[0].geometry.coordinates[0][0] == [-180, 0]
+    assert len(polygon.features[0].geometry.coordinates[0]) == 5
