romatt/exseas_explorer

commit ae41b34f5d9fdc1dc9683a8fb4ea184d033fe3fa

reorder sidebar (#69)

exseas_explorer/app.py
@@ -388,80 +388,64 @@ maprow = html.Div(
                     style={"right": "260px"},
                 ),
                 dbc.Col(
-                    [
+                    children=[
                         html.Div(
-                            title="Polygon details",
-                            children=[
-                                html.Div(
-                                    children=[poly_table],
-                                    id="polygon-table",
-                                ),
-                                html.Div(
+                            children=[poly_table],
+                            id="polygon-table",
+                        ),
+                        # empty container to fill the space between table and buttons
+                        html.Div(style={"flex": "1 1 auto"}),
+                        html.Div(
+                            [
+                                dbc.Button("Information \u2753", id="open", n_clicks=0),
+                                dbc.Modal(
                                     [
-                                        dbc.Button(
-                                            "Information \u2753", id="open", n_clicks=0
-                                        ),
-                                        dbc.Modal(
-                                            [
-                                                dbc.ModalHeader(
-                                                    dbc.ModalTitle(MODAL_TITLE)
-                                                ),
-                                                dbc.ModalBody(MODAL_CONTENT),
-                                                dbc.ModalFooter(
-                                                    dbc.Button(
-                                                        "Close",
-                                                        id="close",
-                                                        className="ms-auto",
-                                                        n_clicks=0,
-                                                    )
-                                                ),
-                                            ],
-                                            id="modal",
-                                            is_open=False,
-                                        ),
-                                    ]
-                                ),
-                                html.Div(
-                                    id="download-netcdf",
-                                    className="download_button",
-                                    children=[
-                                        html.A(
-                                            "Download raw data as netCDF",
-                                            href=f"data/{DEFAULT_SETTING}.nc",
-                                            className="btn btn-success btn-download",
-                                            id="download-netcdf-anchor",
+                                        dbc.ModalHeader(dbc.ModalTitle(MODAL_TITLE)),
+                                        dbc.ModalBody(MODAL_CONTENT),
+                                        dbc.ModalFooter(
+                                            dbc.Button(
+                                                "Close",
+                                                id="close",
+                                                className="ms-auto",
+                                                n_clicks=0,
+                                            )
                                         ),
                                     ],
+                                    id="modal",
+                                    is_open=False,
                                 ),
-                                html.Div(
-                                    id="download-json",
-                                    className="download_button",
-                                    children=[
-                                        html.Div(
-                                            [
-                                                html.Button(
-                                                    "Download current selection as GeoJSON",
-                                                    id="btn-json-download",
-                                                    className="btn btn-success btn-download",
-                                                ),
-                                                dcc.Download(
-                                                    id="download-json-component"
-                                                ),
-                                            ]
-                                        )
-                                    ],
+                            ]
+                        ),
+                        html.Div(
+                            id="download-netcdf",
+                            className="download_button",
+                            children=[
+                                html.A(
+                                    "Download raw data as netCDF",
+                                    href=f"data/{DEFAULT_SETTING}.nc",
+                                    className="btn btn-success btn-download",
+                                    id="download-netcdf-anchor",
                                 ),
                             ],
-                            id="right-collapse",
-                        )
+                        ),
+                        html.Div(
+                            id="download-json",
+                            className="download_button",
+                            children=[
+                                html.Div(
+                                    [
+                                        html.Button(
+                                            "Download current selection as GeoJSON",
+                                            id="btn-json-download",
+                                            className="btn btn-success btn-download ",
+                                        ),
+                                        dcc.Download(id="download-json-component"),
+                                    ]
+                                )
+                            ],
+                        ),
                     ],
                     id="sidebar_column",
-                    style={
-                        "width": "249px",
-                        "display": "block",
-                        "flex": "none",
-                        "right": "0px",
-                    },
                     className="bg-light",
                 ),
             ],

exseas_explorer/assets/style.css
@@ -100,14 +100,20 @@ h6 {
 }
 
 #sidebar_column {
-    padding:0;
-    z-index:1000;
-    position: fixed;
+    padding:2px;
     border-left: 1px solid rgb(211, 211, 211);
+    position: fixed;
+    height: calc(100vh - 240px);
+    width: 249px;
+    right: 0px;
+    z-index:1000;
+    display: flex;
+    flex-flow: column nowrap;
+    align-items: stretch;
 }
 
-#polygon-table {
-    height: calc(100vh - 400px);
+#map_column {
+    height: calc(100vh - 240px);
 }
 
 .cell-table {
@@ -148,6 +154,10 @@ h6 {
 /* SMALL SCREENS */
 @media only screen and (max-width: 990px) {
 
+    #sidebar_column {
+        height: calc(100vh - 307px);
+    }
+
     #map_column {
         height: calc(100vh - 307px);
     }
@@ -177,6 +187,10 @@ h6 {
     }
 
     @media only screen and (max-width: 550px) {
+        #sidebar_column {
+            height: calc(100vh - 600px);
+        }
+
         #map_column {
             height: calc(100vh - 600px);
         }

commit f74855b2c68985abc1ab4def12d04fa742da9958

fix: toggle_sidebar (regression from #69)

exseas_explorer/app.py
@@ -447,6 +447,9 @@ maprow = html.Div(
                     ],
                     id="sidebar_column",
                     className="bg-light",
+                    style={ # required by toggle_sidebar callback
+                        "display": "flex",
+                    }
                 ),
             ],
         )
@@ -663,7 +666,7 @@ def download_geojson(patches, parameter_value, parameter_option, season_value, _
 def toggle_sidebar(n_clicks, sidebar_style, toggle_style, button):
     if n_clicks:
         if sidebar_style["display"] == "none":
-            sidebar_style["display"] = "block"
+            sidebar_style["display"] = "flex"
             toggle_style["right"] = "260px"
             button = ["❯"]
         else:

exseas_explorer/assets/style.css
@@ -107,7 +107,7 @@ h6 {
     width: 249px;
     right: 0px;
     z-index:1000;
-    display: flex;
+    /* display: flex; set inline -> required by toggle_sidebar callback */
     flex-flow: column nowrap;
     align-items: stretch;
 }